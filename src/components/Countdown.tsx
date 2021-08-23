5b8eaedc978cffbb127075d5ccb8ec90c8c6f4f7
kaiique535 <82481212+kaiique535@users.noreply.github.com>
2021-08-23 13:41:11 +0100

    change set time the 0.1 for 25

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -4,83 +4,94 @@ import style from "../styles/components/Countdown.module.css";
 let countdownTimeout: NodeJS.Timeout;
 
 export function Countdown() {
+  /* Declaração de uma estado de React(useState)- Com isso automaticamente sera importado uma biblioteca do react no inicio do codigo */
+  const [time, setTime] = useState(
+    25 * 60
+  ); /* Como a contagem sera um declinio de segundos, iniciaremos a contagem em segundos - nome= time e setTime vai modificar a contagem */
+  const [isActive, setIsActive] = useState(false);
+  const [hasFinished, setHasFinished] = useState(false);
 
-    /* Declaração de uma estado de React(useState)- Com isso automaticamente sera importado uma biblioteca do react no inicio do codigo */
-    const [time, setTime] = useState(0.1 * 60); /* Como a contagem sera um declinio de segundos, iniciaremos a contagem em segundos - nome= time e setTime vai modificar a contagem */
-    const [isActive, setIsActive] = useState(false);
-    const [hasFinished, setHasFinished] = useState(false);
+  const minutes = Math.floor(
+    time / 60
+  ); /* Aqui voce tem o time calculado na linh acima para formar os minutos, Quando tiver um valor quebrado ultilizamos Math.floor para arredondar o valor para baixo.  */
+  const seconds =
+    time % 60; /* O numero de segundos é o 'resto' da divisão dos minutos, */
 
+  const [minuteLeft, minuteRight] = String(minutes)
+    .padStart(2, "0")
+    .split(
+      ""
+    ); /* padStart está garantinho que mesmo que tenhamos abaixo de 10 teremos '2'Numeros (Exemplo '09' - E com o split vazio temos a separação dos numeros. Gerando os Minutos Left e Right)  */
+  const [SecondLeft, SecondRight] = String(seconds)
+    .padStart(2, "0")
+    .split(""); /* Mesma aplicação da linha acima mas agora em segundos */
 
-    const minutes = Math.floor(time / 60);/* Aqui voce tem o time calculado na linh acima para formar os minutos, Quando tiver um valor quebrado ultilizamos Math.floor para arredondar o valor para baixo.  */
-    const seconds = time % 60;/* O numero de segundos é o 'resto' da divisão dos minutos, */
+  function startCountdown() {
+    setIsActive(true);
+  }
 
+  function resetCountdown() {
+    clearTimeout(countdownTimeout);
+    setIsActive(false);
+    setTime(25 * 60);
+  }
 
-    const [minuteLeft, minuteRight] = String(minutes).padStart(2, "0").split(""); /* padStart está garantinho que mesmo que tenhamos abaixo de 10 teremos '2'Numeros (Exemplo '09' - E com o split vazio temos a separação dos numeros. Gerando os Minutos Left e Right)  */
-    const [SecondLeft, SecondRight] = String(seconds).padStart(2, "0").split("");/* Mesma aplicação da linha acima mas agora em segundos */
-
-    function startCountdown() {
-        setIsActive(true);
+  useEffect(() => {
+    if (isActive && time > 0) {
+      countdownTimeout = setTimeout(() => {
+        setTime(time - 1);
+      }, 1000);
+    } else if (isActive && time == 0) {
+      setHasFinished(true);
+      setIsActive(false);
     }
+  }, [isActive, time]);
 
-    function resetCountdown() {
-        clearTimeout(countdownTimeout);
-        setIsActive(false);
-        setTime(0.1 * 60);
-    }
-
-    useEffect(() => {
-        if (isActive && time > 0) {
-            countdownTimeout = setTimeout(() => {
-                setTime(time - 1);
-            }, 1000);
-        } else if (isActive && time == 0) {
-            setHasFinished(true);
-            setIsActive(false);
-        }
-    }, [isActive, time]);
-
-
-    return (
+  return (
+    <div>
+      <div className={style.countdownContainer}>
         <div>
-            <div className={style.countdownContainer}>
-                <div>
-                    {/* Agora dentro das Spans colocamos as variaveis left e Right de cada um dos parametros */}
-                    <span>{minuteLeft}</span>
-                    <span>{minuteRight}</span>
-                </div>
+          {/* Agora dentro das Spans colocamos as variaveis left e Right de cada um dos parametros */}
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
+        </div>
 
-                <span>:</span>
+        <span>:</span>
 
-                <div>
-                    <span>{SecondLeft}</span>
-                    <span>{SecondRight}</span>
-                </div>
-            </div>
+        <div>
+          <span>{SecondLeft}</span>
+          <span>{SecondRight}</span>
+        </div>
+      </div>
 
-            {hasFinished ? (
-                <button
-                    disabled
-                    className={style.countdownButton}
-                >
-                    Ciclo Finalizado...
-                </button>
-            ) :
-
-                (<>{ /* <></>=div que não é exibida dentro do HTML */
-                    isActive ? (<button
-                        type="button"
-                        className={`${style.countdownButton} ${style.countdownButtonIsActive}`}
-                        onClick={resetCountdown}>
-                        Abandonar Ciclo
-                    </button>) : (<button
-                        type="button"
-                        className={style.countdownButton}
-                        onClick={startCountdown}>
-                        iniciar um Ciclo
-                    </button>)
-                }</>)
-            }
-
-        </div >
-    );
+      {hasFinished ? (
+        <button disabled className={style.countdownButton}>
+          Ciclo Finalizado...
+        </button>
+      ) : (
+        <>
+          {
+            /* <></>=div que não é exibida dentro do HTML */
+            isActive ? (
+              <button
+                type="button"
+                className={`${style.countdownButton} ${style.countdownButtonIsActive}`}
+                onClick={resetCountdown}
+              >
+                Abandonar Ciclo
+              </button>
+            ) : (
+              <button
+                type="button"
+                className={style.countdownButton}
+                onClick={startCountdown}
+              >
+                iniciar um Ciclo
+              </button>
+            )
+          }
+        </>
+      )}
+    </div>
+  );
 }
