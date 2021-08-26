ecec0833a67794ebfc5e27571bc0e10402588ff8
kaiique535 <82481212+kaiique535@users.noreply.github.com>
2021-08-26 16:55:26 +0100

    add experienceToNextLevel, currentExperience by  ChallengesContext

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,23 +1,34 @@
+import { useContext } from "react";
 import styles from "../styles/components/ExperienceBar.module.css";
+import { ChallengesContext } from "../contexts/ChallengesContext";
 
 export function ExperienceBar() {
+  const { currentExperience, experienceToNextLevel } =
+    useContext(ChallengesContext);
+
+  const percentToNextLevel =
+    Math.round(currentExperience * 100) / experienceToNextLevel;
+
   return (
     <header className={styles.experienceBar}>
       {" "}
       {/* escificação para o renderizar no CSS */}
-      <span>0 xp</span>
+      <span> 0 xp</span>
       {/* Valor minimo da experienceBar */}
       <div>
-        <div style={{ width: "50%" }} />
+        <div style={{ width: `${percentToNextLevel}%` }} />
         {/* Barra verde que está definida em 50% de progresso */}
 
-        <span className={styles.currentExperience} style={{ left: "50%" }}>
+        <span
+          className={styles.currentExperience}
+          style={{ left: `${percentToNextLevel}%` }}
+        >
           {" "}
           {/* /* escificação para o renderizar no CSS e também definindo o valor progresso da barra verde (300xp) */}
-          300 xp
+          {currentExperience} xp
         </span>
       </div>
-      <span>600</span>
+      <span>{experienceToNextLevel}xp</span>
       {/* Valor da maximo da experienceBar */}
     </header>
   );
