2cad11008b3b3407f132fab4e7ee872ec79eefd5
kaiique535 <82481212+kaiique535@users.noreply.github.com>
2021-08-26 16:53:17 +0100

    Add ChallengesCompleted by challegesContext

--- src/components/CompleteChallenges.tsx
+++ src/components/CompleteChallenges.tsx
@@ -1,10 +1,14 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
 import styles from "../styles/components/CompleteChallenges.module.css";
 
 export function CompleteChallenges() {
+  const { challengesCompleted } = useContext(ChallengesContext);
+
   return (
     <div className={styles.completeChallengesContainer}>
-      <span>DesaFios Completos</span>
-      <span>5</span>
+      <span>Desafios Completos</span>
+      <span>{challengesCompleted}</span>
     </div>
   );
 }
