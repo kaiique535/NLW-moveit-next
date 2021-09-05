47b6662bcce7cb58bd98ecd4a14abc8f72e563a2
root <root@VICTORINO.localdomain>
2021-09-05 23:21:18 +0100

    Functions moved for CountdownContext

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,60 +1,25 @@
 import { useContext, useEffect, useState } from "react";
-import { ChallengesContext } from "../contexts/ChallengesContext";
+import { CountdownContext } from "../contexts/CountdownContext";
 import style from "../styles/components/Countdown.module.css";
 
-let countdownTimeout: NodeJS.Timeout;
-
 export function Countdown() {
-  const { startNewChallenge } = useContext(ChallengesContext);
-  /* Declaração de uma estado de React(useState)- Com isso automaticamente sera importado uma biblioteca do react no inicio do codigo */
-  const [time, setTime] = useState(
-    0.05 * 60
-  ); /* Como a contagem sera um declinio de segundos, iniciaremos a contagem em segundos - nome= time e setTime vai modificar a contagem */
-  const [isActive, setIsActive] = useState(false);
-  const [hasFinished, setHasFinished] = useState(false);
+  const {
+    minutes,
+    seconds,
+    hasFinished,
+    isActive,
+    startCountdown,
+    resetCountdown,
+  } = useContext(CountdownContext);
 
-  const minutes = Math.floor(
-    time / 60
-  ); /* Aqui voce tem o time calculado na linh acima para formar os minutos, Quando tiver um valor quebrado ultilizamos Math.floor para arredondar o valor para baixo.  */
-  const seconds =
-    time % 60; /* O numero de segundos é o 'resto' da divisão dos minutos, */
-
-  const [minuteLeft, minuteRight] = String(minutes)
-    .padStart(2, "0")
-    .split(
-      ""
-    ); /* padStart está garantinho que mesmo que tenhamos abaixo de 10 teremos '2'Numeros (Exemplo '09' - E com o split vazio temos a separação dos numeros. Gerando os Minutos Left e Right)  */
-  const [SecondLeft, SecondRight] = String(seconds)
-    .padStart(2, "0")
-    .split(""); /* Mesma aplicação da linha acima mas agora em segundos */
-
-  function startCountdown() {
-    setIsActive(true);
-  }
-
-  function resetCountdown() {
-    clearTimeout(countdownTimeout);
-    setIsActive(false);
-    setTime(0.05 * 60);
-  }
-
-  useEffect(() => {
-    if (isActive && time > 0) {
-      countdownTimeout = setTimeout(() => {
-        setTime(time - 1);
-      }, 1000);
-    } else if (isActive && time == 0) {
-      setHasFinished(true);
-      setIsActive(false);
-      startNewChallenge();
-    }
-  }, [isActive, time]);
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, "0").split(""); // padStart está garantinho que mesmo que tenhamos abaixo de 10 teremos '2'Numeros (Exemplo '09' - E com o split vazio temos a separação dos numeros. Gerando os Minutos Left e Right)  */
+  const [SecondLeft, SecondRight] = String(seconds).padStart(2, "0").split(""); // Mesma aplicação da linha acima mas agora em segundos */
 
   return (
     <div>
       <div className={style.countdownContainer}>
         <div>
-          {/* Agora dentro das Spans colocamos as variaveis left e Right de cada um dos parametros */}
+          {/*  Agora dentro das Spans colocamos as variaveis left e Right de cada um dos parametros */}
           <span>{minuteLeft}</span>
           <span>{minuteRight}</span>
         </div>
@@ -74,7 +39,7 @@ export function Countdown() {
       ) : (
         <>
           {
-            /* <></>=div que não é exibida dentro do HTML */
+            // <></>=div que não é exibida dentro do HTML
             isActive ? (
               <button
                 type="button"
