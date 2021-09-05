77f00b7027556cd3b72a12793943ba4233de619d
root <root@VICTORINO.localdomain>
2021-09-05 21:18:56 +0100

    Add countdownProvider. The Countdownprovider envolve section, for apply on initial page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { CompleteChallenges } from "../components/CompleteChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+import { CountdownProvider } from "../contexts/CountdownContext";
 
 import styles from "../styles/pages/Home.module.css";
 
@@ -15,16 +16,18 @@ export default function Home() {
         <title>Inicio | Move.it </title>
       </head>
       <ExperienceBar /> {/* Componente importado  */}
-      <section>
-        <div>
-          <Profile />
-          <CompleteChallenges />
-          <Countdown />
-        </div>
-        <div>
-          <ChallengeBox />
-        </div>
-      </section>
-    </div>
+      <CountdownProvider>
+        <section>
+          <div>
+            <Profile />
+            <CompleteChallenges />
+            <Countdown />
+          </div>
+          <div>
+            <ChallengeBox />
+          </div>
+        </section>
+      </CountdownProvider>
+    </div >
   );
 }
