28fee8cfe9681e79aace5d3f40ac91d041673a07
root <root@VICTORINO.localdomain>
2021-09-06 21:24:06 +0100

    resove problems

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
 import { useContext } from "react";
 import { ChallengesContext } from "../contexts/ChallengesContext";
 import { CountdownContext } from "../contexts/CountdownContext";
@@ -5,13 +7,13 @@ import styles from "../styles/components/ChallengeBox.module.css";
 import { CompleteChallenges } from "./CompleteChallenges";
 
 export function ChallengeBox() {
-  const { activeChallenge, resetChallenge, completeChallenge } = useContext(ChallengesContext);
-  const { resetCountdown } = useContext(CountdownContext)
+  const { activeChallenge, resetChallenge, completeChallenge } =
+    useContext(ChallengesContext);
+  const { resetCountdown } = useContext(CountdownContext);
 
   function handleChallengeSucceeded() {
     completeChallenge();
     resetCountdown();
-
   }
 
   function handleChallengeFailed() {
@@ -52,7 +54,7 @@ export function ChallengeBox() {
           <strong> Finalize um ciclo para receber um novo desafio</strong>
           <p>
             <img src="icons/level-up.svg" alt="Level Up" />
-            Avance de level completando desafios.
+            Avance de level Completando Desafios.
           </p>
         </div>
       )}
