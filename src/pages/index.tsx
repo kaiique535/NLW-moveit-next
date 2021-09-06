4a1d5211c61660aaa0b09db1d2828cd23b20e8c1
root <root@VICTORINO.localdomain>
2021-09-06 20:39:10 +0100

    Creating HomeProps to implement and exibe leveUp Mensage. Creating const getServerSidesProps to manipulate cookies and save state of aplication

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,29 +5,55 @@ import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 import { CountdownProvider } from "../contexts/CountdownContext";
+import { GetServerSideProps } from "next";
 
 import styles from "../styles/pages/Home.module.css";
+import { ChallengesProvider } from "../contexts/ChallengesContext";
 
-export default function Home() {
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
+
+export default function Home(props: HomeProps) {
   return (
-    <div className={styles.container}>
-      {/* Colocando ExperienceBar dentro de um container para poder ajustar no CSS */}
-      <head>
-        <title>Inicio | Move.it </title>
-      </head>
-      <ExperienceBar /> {/* Componente importado  */}
-      <CountdownProvider>
-        <section>
-          <div>
-            <Profile />
-            <CompleteChallenges />
-            <Countdown />
-          </div>
-          <div>
-            <ChallengeBox />
-          </div>
-        </section>
-      </CountdownProvider>
-    </div >
+    <ChallengesProvider
+      level={props.level}
+      currentExperience={props.currentExperience}
+      challengesCompleted={props.challengesCompleted}
+    >
+      <div className={styles.container}>
+        {/* Colocando ExperienceBar dentro de um container para poder ajustar no CSS */}
+        <head>
+          <title>Inicio | Move.it </title>
+        </head>
+        <ExperienceBar /> {/* Componente importado  */}
+        <CountdownProvider>
+          <section>
+            <div>
+              <Profile />
+              <CompleteChallenges />
+              <Countdown />
+            </div>
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
+      </div>
+    </ChallengesProvider>
   );
 }
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  // chamada API
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies;
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted),
+    },
+  };
+};
